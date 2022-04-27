876fb48d6bddf0dff56bdeece375da722f0846f2
Alexander Schlögl <alexander.schloegl@uibk.ac.at>
2022-04-27 14:07:32 +0200

    Add CRUD operations

--- FruitShop/Controllers/ProductController.cs
+++ FruitShop/Controllers/ProductController.cs
@@ -8,9 +8,49 @@ namespace FruitShop.Controllers;
 [Route("[controller]")]
 public class ProductController : ControllerBase
 {
-    [HttpGet(Name = "products")]
+    [HttpGet]
     public IEnumerable<Product> Get()
     {
         return ProductService.GetAll();
+    }
+
+    [HttpGet("{id:int}")]
+    public ActionResult<Product> Get(int id)
+    {
+        var product = ProductService.Get(id);
+        if (product != null)
+            return product;
+        
+        return NotFound();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Product product)
+    {
+        ProductService.Add(product);
+        return CreatedAtAction(nameof(Create), new { id = product.Id }, product);
+    }
+
+    [HttpPut("{id:int}")]
+    public IActionResult Update(int id, Product product)
+    {
+        if (id != product.Id)
+            return BadRequest();
+
+        if (ProductService.Get(id) is null)
+            return NotFound();
+        
+        ProductService.Update(product);
+        return NoContent();
+    }
+
+    [HttpDelete("{id:int}")]
+    public IActionResult Delete(int id)
+    {
+        if (ProductService.Get(id) is null)
+            return NotFound();
+        
+        ProductService.Delete(id);
+        return NoContent();
     }
 }
